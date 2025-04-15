53fcf88b5d26e0a29de811de353ec3ae05b109fd
antoniovsegala <antoniosegala023@gmail.com>
2025-04-15 20:04:17 -0300

    - Exercicio basico de Java. - Entrada de dados. - Processamento de dados. - Saida de dados.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,10 +8,13 @@ public class Main {
 
         int x, y, soma;
 
-
+        System.out.println("Digite o primeiro número para somar:");
         x = sc.nextInt();
+        System.out.println("Digite o segundo número para somar:");
         y = sc.nextInt();
+
         soma = x + y;
+
         System.out.printf("O resultado da soma é " + soma);
 
         sc.close();
